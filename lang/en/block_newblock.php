82b244fb25ac2b99156b6c6ecf4abe01bb8777c4
Daniel Neis <danielneis@gmail.com>
2013-01-17 15:11:38 -0200

    Adding missing strings

--- lang/en/block_newblock.php
+++ lang/en/block_newblock.php
@@ -23,6 +23,11 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+$string['blockstring'] = 'Block string';
+$string['descconfig'] = 'Description of the config section';
+$string['descfoo'] = 'Config description';
+$string['headerconfig'] = 'Config section header';
+$string['labelfoo'] = 'Config label';
 $string['newblock:addinstance'] = 'Add a newblock block';
 $string['newblock:myaddinstance'] = 'Add a newblock block to my moodle';
 $string['pluginname'] = 'Newblock';
