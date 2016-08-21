11086d9a317cc6cd1fea167a1d8e00190d4e6df1
Nadav Kavalerchik <nadavkav@gmail.com>
2016-08-21 11:47:37 +0300

    PARAM_MULTILANG is deprecated in favor of PARAM_TEXT
    Tiny update (more info: https://github.com/moodle/moodle/blob/master/lib/moodlelib.php#L286-L290)

--- edit_form.php
+++ edit_form.php
@@ -10,7 +10,7 @@ class block_newblock_edit_form extends block_edit_form {
         // A sample string variable with a default value.
         $mform->addElement('text', 'config_text', get_string('blockstring', 'block_newblock'));
         $mform->setDefault('config_text', 'default value');
-        $mform->setType('config_text', PARAM_MULTILANG);        
+        $mform->setType('config_text', PARAM_TEXT);        
 
     }
 }
